20a4c1c845f47cfe95e4951ec73f928f4b64b860
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-08-08 11:47:24 +0000

    Import of MANWAR/WWW-Google-CustomSearch-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Google-CustomSearch
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MANWAR/WWW-Google-CustomSearch-0.02.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/WWW/Google/CustomSearch.pm
+++ lib/WWW/Google/CustomSearch.pm
@@ -58,11 +58,11 @@ WWW::Google::CustomSearch - Interface to Google JSON/Atom Custom Search.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 Readonly my $API_VERSION => 'v1';
 Readonly my $BASE_URL    => "https://www.googleapis.com/customsearch/$API_VERSION";
 
